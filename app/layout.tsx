33ceb38860f7d4b5a55517bd6479b7dbc25bc206
Marouane Benaddou Idrissi <marouanebenaddouidrissi@Marouanes-MacBook-Pro.local>
2026-04-09 16:32:26 +0100

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <div className="gtranslate_wrapper"></div>
         {children}
+        {/* Microsoft Clarity */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","w923wy7nln");` }} />
       </body>
     </html>
   );
